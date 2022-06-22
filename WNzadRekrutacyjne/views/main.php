f06f3910c57c8a8619b112078f4b15be1fbfae29
werexa <werexa200@gmail.com>
2022-06-22 23:13:46 +0200

    Add base elements

---
+++ WNzadRekrutacyjne/views/main.php
@@ -0,0 +1,21 @@
+<html>
+<?php include("header.php") ?>
+
+<body>
+    <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+    <?php include("menu.php") ?>
+        <main role="main" class="inner cover">
+            <h1 class="cover-heading">Hej, chcę do was dołączyć</h1>
+            <p class="lead">Wejdź w jedną z zakładek aby zobaczyć moją pracę</p>
+            <p class="lead">
+                <a href="#" class="btn btn-lg btn-secondary">Zobacz na przykład klientów</a>
+            </p>
+        </main>
+        <?php include("footer.php") ?>
+
+    </div>
+
+
+</body>
+
+</html>
